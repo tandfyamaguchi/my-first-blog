27fc4ec01d3bfdb82396d0c31d039ce2a4c4be10
yamaguchi <tfkiyuko1132@gmail.com>
2019-01-22 13:52:12 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # http:127.0.0.1:8000/post/<pkの値>/で表示
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
